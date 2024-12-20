dbaf633eadef1ce9f6ae6f90027b90f5f3b174a9
ayoubMah <ayoubelmahjouby9@gmail.com>
2024-12-20 23:34:38 +0100

    refactor: comment out unused code and improve data type examples

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,18 +4,29 @@ using namespace std ;
 
 
 int main() {
-  cout << "=========== WELLCOME TO OUR APP ==============\n" ;
-  short int s = 10;
-  cout << sizeof(s) << "\n";
-  unsigned int a = -12 ;
-  cout << a << "\n" ;
+  // cout << "=========== WELLCOME TO OUR APP ==============\n" ;
+  // short int s = 10;
+  // cout << sizeof(s) << "\n";
+  // unsigned int a = -12 ;
+  // cout << a << "\n" ;
 
-  using bigint = long long int ; // using alias with using
+  // using bigint = long long int ; // using alias with using
 
-  typedef long long int lli ; // using alias with typedef
+  // typedef long long int lli ; // using alias with typedef
 
-  lli z = 1234567890987654321 ;
-  cout << z << "\n" ;
+  // lli z = 1234567890987654321 ;
+  // cout << z << "\n" ;
+
+  // ================= Converting data types ====================
+  int x = 10 ;
+  float y = 10.5 ;
+
+   char z = 'C' ; // cuz the haski value of C is 67
+   int d = 10 ;
+   cout << z + d << "\n" ;
+   cout << (int)z << "\n" ;
+   //cout << (int)z << "\n" ;
+
 
   return 0;
 }
